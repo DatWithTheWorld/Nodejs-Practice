f448e2025e365a7acaa590cc2ecd4ba732303c54
= <=>
2024-07-27 13:16:54 +0700

    create

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,12 @@
-const newRouter = require('./news');
-const siteRouter = require('./site');
+const newRouter = require("./news");
+const coursesRouter = require("./courses");
+const siteRouter = require("./site");
 
 function route(app) {
-   app.use('/news', newRouter);
-   app.use('/', siteRouter);
-   // Default route
+  app.use("/courses", coursesRouter);
+  app.use("/news", newRouter);
+  app.use("/", siteRouter);
+  // Default route
 }
 
 module.exports = route;
